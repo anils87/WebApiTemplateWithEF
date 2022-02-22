31547dd97d1dff1c901551bfac7fa4e30669f6aa
anils87 <anils15787@gmail.com>
2022-02-22 11:42:37 +0530

    Uploading files

---
+++ Startup.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPIProject.Data;
+using WebAPIProject.Data.Repo;
+using WebAPIProject.Interfaces;
+using AutoMapper;
+using WebAPIProject.Helpers;
+using WebAPIProject.Extensions;
+using WebAPIProject.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace WebAPIProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            
+            services.AddCors();
+            // AutoMapper to map the Model Entity with DTOs
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+
+            //Inject Repository here using dependency Injection
+            // same instance of DB context Will be used for same http request.
+            //services.AddScoped<ICityRepository, CityRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            var secretKey = Configuration.GetSection("AppSettings:Key").Value;
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt=>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        IssuerSigningKey = key
+                    };
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Web API Project",
+                    Description = "This is sample dotnet core api project",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Anil Yadav",
+                        Email = string.Empty,
+                        //Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "NA",
+                        //Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API Project");
+            });
+            // Inject Exception Handler globally
+            app.ConfigureExceptionHandler(env);
+
+            // Exception Middleware to handle application
+            // app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseCors(c => c.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseAuthentication();
+            //app.UseAuthorization();
+
+            app.UseMvc();
+
+           
+            
+            
+        }
+    }
+}
